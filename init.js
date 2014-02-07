e7fae361cda2ba09ce4780c60746c7ddf764432c
psramirez <pablillo.es@gmail.com>
2014-02-08 00:13:24 +0100

    Llamando al Screen de bienvenida

---
+++ init.js
@@ -0,0 +1,12 @@
+iris.path = {
+    welcome: {js: "./screen/welcome.js", html: "./screen/welcome.html"}
+};
+
+
+$(document).ready(
+    function () {
+        iris.baseUri("./"); 
+        iris.welcome(iris.path.welcome.js);
+    }
+);
+
